64b7e1fb496c62434d831f4f1cf457d9b1bcfe78
Alex Thalhammer <alex@angular-software.at>
2021-07-01 12:58:24 +0200

    Multi-Field-Validatoren

--- src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,32 +1,29 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from '../../../environments/environment';
 import { FlightService } from '../flight-search/flight.service';
 import { Flight } from '../../entities/flight';
 import { validateCity } from '../../shared/validation/city-validator';
 import { validCities } from '../../shared/validation/city-validator.directive';
+import { validateRoundTrip } from '../../shared/validation/form-round-trip';
 
 @Component({
   selector: 'flight-edit',
   templateUrl: './flight-edit.component.html',
   styleUrls: ['./flight-edit.component.css']
 })
-export class FlightEditComponent implements OnChanges {
+export class FlightEditComponent implements OnChanges, OnInit {
   @Input() flight: Flight;
 
   @Output() saved: EventEmitter<Flight> = new EventEmitter<Flight>();
 
   editForm: FormGroup;
+  isEditFormInitialized = false;
 
   constructor(private formBuilder: FormBuilder, private flightService: FlightService) {}
 
   ngOnChanges(): void {
-    this.editForm = this.formBuilder.group({
-      id: [this.flight.id],
-      from: [this.flight.from, [Validators.required, Validators.minLength(3), Validators.maxLength(15), validateCity(validCities)]],
-      to: [this.flight.to, [Validators.required, Validators.minLength(3), Validators.maxLength(15), validateCity(validCities)]],
-      date: [this.flight.date, [Validators.required, Validators.minLength(21), Validators.maxLength(35)]]
-    });
+    this.initForm();
 
     this.logEditForm();
 
@@ -37,10 +34,29 @@ export class FlightEditComponent implements OnChanges {
     }
   }
 
+  ngOnInit(): void {
+    if (!this.isEditFormInitialized) {
+      this.initForm();
+    }
+
+    this.editForm.validator = validateRoundTrip;
+  }
+
   save(): void {
     this.logEditForm();
 
     this.saved.emit(this.editForm.value);
+  }
+
+  private initForm(): void {
+    this.editForm = this.formBuilder.group({
+      id: [this.flight.id],
+      from: [this.flight.from, [Validators.required, Validators.minLength(3), Validators.maxLength(15), validateCity(validCities)]],
+      to: [this.flight.to, [Validators.required, Validators.minLength(3), Validators.maxLength(15), validateCity(validCities)]],
+      date: [this.flight.date, [Validators.required, Validators.minLength(21), Validators.maxLength(35)]]
+    });
+
+    this.isEditFormInitialized = true;
   }
 
   private logEditForm(): void {
---
+++ src/app/shared/validation/form-round-trip.ts
@@ -0,0 +1,14 @@
+import { AbstractControl, FormGroup, ValidationErrors } from '@angular/forms';
+
+export function validateRoundTrip(formGroup: FormGroup): ValidationErrors {
+  let from = formGroup.controls['from'];
+  let to = formGroup.controls['to'];
+
+  if (!from || !to) return null;
+
+  if (from.value === to.value) {
+    return { roundTrip: true };
+  }
+
+  return null;
+}
