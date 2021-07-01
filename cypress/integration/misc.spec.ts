3ac04efb02e4265fce2c7234a6a8ebf6a65db274
Alex Thalhammer <alex@angular-software.at>
2021-07-01 16:12:36 +0200

    E2E Testing examples

---
+++ cypress/integration/misc.spec.ts
@@ -0,0 +1,22 @@
+describe('misc e2e test', () => {
+  it('should do a sanity check', () => {
+    cy.visit('');
+  });
+
+  it('should load page below 1 second', () => {
+    cy.visit('/', {
+      onBeforeLoad: (win) => {
+        win.performance.mark('start-loading');
+      },
+      onLoad: (win) => {
+        win.performance.mark('end-loading');
+      }
+    })
+      .its('performance')
+      .then((p) => {
+        p.measure('pageLoad', 'start-loading', 'end-loading');
+        const measure = p.getEntriesByName('pageLoad')[0];
+        expect(measure.duration).to.be.most(1000);
+      });
+  });
+});
---
+++ cypress/integration/page.spec.ts
@@ -0,0 +1,28 @@
+describe('holidays e2e test', () => {
+  it('should do a sanity check', () => {
+    cy.visit('');
+  });
+
+  it('should do an implicit subject assertion', () => {
+    cy.visit('');
+    cy.get('h1').should('have.text', 'Hello World!');
+  });
+
+  it('should do an explicit subject assertion', () => {
+    cy.visit('');
+    cy.get('form input[name="from"]').type('Graz');
+    cy.get('form input[name="to"]').type('Hamburg');
+    cy.get('form .btn').should(($button) => {
+      expect($button).to.not.have.attr('disabled', 'disabled');
+    });
+  });
+
+  it('should do a form submit and count length of flight cards', () => {
+    cy.visit('');
+    cy.get('form input[name="from"]').type('Graz');
+    cy.get('form input[name="to"]').type('Hamburg');
+    cy.get('form .btn').click();
+
+    cy.get('flight-card').should('have.length', 11);
+  });
+});
