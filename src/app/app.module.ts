3691fd306f7cc92081ec4d1a5d49c40f59463a24
Alex Thalhammer <alex@angular-software.at>
2021-07-02 06:37:18 +0200

    Routing implementieren

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { FlightBookingModule } from './flight-booking/flight-booking.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, FormsModule, FlightBookingModule],
-  declarations: [AppComponent, SidebarComponent, NavbarComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule, FlightBookingModule, RouterModule.forRoot(APP_ROUTES)],
+  declarations: [AppComponent, SidebarComponent, NavbarComponent, HomeComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/flight-booking/flight-booking.module.ts
+++ src/app/flight-booking/flight-booking.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { SharedModule } from '../shared/shared.module';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { FlightCardComponent } from './flight-card/flight-card.component';
 import { FlightStatusToggleComponent } from './flight-status-toggle/flight-status-toggle.component';
 import { FlightValidationErrorsComponent } from './flight-validation-errors/flight-validation-errors.component';
 import { FlightEditComponent } from './flight-edit/flight-edit.component';
+import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
+
+import { FLIGHT_BOOKING_ROUTES } from './flight-booking.routes';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, SharedModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, SharedModule, ReactiveFormsModule, RouterModule.forChild(FLIGHT_BOOKING_ROUTES)],
   declarations: [
     FlightSearchComponent,
     FlightCardComponent,
     FlightStatusToggleComponent,
     FlightValidationErrorsComponent,
-    FlightEditComponent
+    FlightEditComponent,
+    PassengerSearchComponent
   ],
   providers: [],
   exports: [FlightSearchComponent]
